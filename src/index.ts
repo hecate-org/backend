3d11774a6e5db9d608951e876604a66c3c3884db
Ward De Coster <warddecoster2003@gmail.com>
2021-11-13 11:36:25 +0100

    utils initial

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const { Server, Socket } = require("socket.io");
 const io = new Server(server, { cors: "*" });
 
 const eventHandler = require("./events");
-const actionHandler = require("./actions")
+const actionHandler = require("./utils")
 
 server.listen(4000, async () => {
   console.log("listening to request on port 4000");
